03f777f367d7ca89f84038265cc416baee7a43ec
KasMichta <kacper.michta33@gmail.com>
2023-10-22 21:27:19 +0100

    formatting

--- day_05/day_05.go
+++ day_05/day_05.go
@@ -5,8 +5,8 @@ import (
 	"os"
 	"regexp"
 	"slices"
+	"strconv"
 	"strings"
-    "strconv"
 )
 
 func readLines(file string) []string {
@@ -31,35 +31,35 @@ func readCrates(lines []string) map[int][]string {
 	stacks := make(map[int][]string)
 
 	for stkid, stk := range stackNumbers {
-        //index of stack column
+		//index of stack column
 		column := stk[0]
-        var crates []string
+		var crates []string
 
-        for i := 0; i < (breakLine - 1) ; i++  {
-		    crate := string(lines[i][column])
-            if crate != " " {
-                crates = append(crates, crate)
-            }
+		for i := 0; i < (breakLine - 1); i++ {
+			crate := string(lines[i][column])
+			if crate != " " {
+				crates = append(crates, crate)
+			}
 		}
-        stacks[stkid+1] = crates
+		stacks[stkid+1] = crates
 	}
 	return stacks
 }
 
 func readProcedure(procedureString string) map[string]int {
-    re:= regexp.MustCompile(`\d+`)
-    steps := re.FindAllString(procedureString, -1)
-    procedure := make(map[string]int)
+	re := regexp.MustCompile(`\d+`)
+	steps := re.FindAllString(procedureString, -1)
+	procedure := make(map[string]int)
 
-    procedure["move"], _ = strconv.Atoi(steps[0])
-    procedure["from"], _ = strconv.Atoi(steps[1])
-    procedure["to"], _ = strconv.Atoi(steps[2])
+	procedure["move"], _ = strconv.Atoi(steps[0])
+	procedure["from"], _ = strconv.Atoi(steps[1])
+	procedure["to"], _ = strconv.Atoi(steps[2])
 
-    return procedure
+	return procedure
 }
 
 func main() {
 	lines := readLines(os.Args[1])
 	fmt.Printf("%v\n", readCrates(lines))
-    fmt.Printf("%v\n", readProcedure(lines[10]))
+	fmt.Printf("%v\n", readProcedure(lines[10]))
 }
