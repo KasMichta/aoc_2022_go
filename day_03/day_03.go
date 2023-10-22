dac2af9ec4b7afcda4f0a300caaa172a7eaf85f7
KasMichta <kacper.michta33@gmail.com>
2023-10-22 11:55:21 +0100

    Part 2 complete

--- day_03/day_03.go
+++ day_03/day_03.go
@@ -18,7 +18,7 @@ func main() {
 		prioInt++
 	}
 
-	fmt.Printf("priorities:\n%v\n", priorities)
+	//fmt.Printf("priorities:\n%v\n", priorities)
 
 	//load file
 	data, err := os.ReadFile(os.Args[1])
@@ -30,7 +30,7 @@ func main() {
 	//omit the last as it will be an empty string
 	splitLines = splitLines[:len(splitLines)-1]
 
-	//splitLines = splitLines[:5]
+	//splitLines = splitLines[:9]
 
 	var prioSum int
 	for _, line := range splitLines {
@@ -39,14 +39,36 @@ func main() {
 
 		for _, char := range comp1 {
 			if strings.ContainsRune(comp2, char) {
-                //remove char from both avoid multihits
-                comp1 = strings.ReplaceAll(comp1, string(char), "")
-                comp2 = strings.ReplaceAll(comp2, string(char), "")
+				//remove char from both avoid multihits
+				comp1 = strings.ReplaceAll(comp1, string(char), "")
+				comp2 = strings.ReplaceAll(comp2, string(char), "")
 
 				prioSum += priorities[string(char)]
 			}
 		}
 	}
-    fmt.Printf("Sum of Priorities: %v\n", prioSum)
-    
+	fmt.Printf("Sum of Priorities: %v\n", prioSum)
+
+	//Part 2
+	var badgeSum int
+
+	for i := 0; i <= len(splitLines)-3; i = i + 3 {
+		members := []string{splitLines[i], splitLines[i+1], splitLines[i+2]}
+
+		for _, char := range members[0] {
+			if strings.ContainsRune(members[1], char) {
+				if strings.ContainsRune(members[2], char) {
+                    //iterating over range to update 'member' won't update the members values
+                    //need to index members slice with the member loop var
+					for member := range members {
+						members[member] = strings.ReplaceAll(members[member], string(char), "")
+					}
+					badgeSum += priorities[string(char)]
+				}
+			}
+		}
+	}
+
+	fmt.Printf("Sum of Badges: %v\n", badgeSum)
+
 }
