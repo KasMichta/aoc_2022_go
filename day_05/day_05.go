2d21805257d464db8212aac06f0a400398955b26
KasMichta <kacper.michta33@gmail.com>
2023-10-24 23:00:01 +0100

    formatting

--- day_05/day_05.go
+++ day_05/day_05.go
@@ -60,35 +60,35 @@ func readProcedure(procedureString string) (move, from, to int) {
 }
 
 func main() {
-    //Part 1
+	//Part 1
 	lines := readLines(os.Args[1])
 	procLines := lines[10:]
 	//fmt.Printf("%v\n", readCrates(lines))
 	//fmt.Printf("%v\n", readProcedure(lines[10]))
 	stacksP1 := readCrates(lines)
-    //Part 2
-    stacksP2 := readCrates(lines)
+	//Part 2
+	stacksP2 := readCrates(lines)
 	for _, line := range procLines {
 		move, from, to := readProcedure(line)
 
 		cratesToMoveP1 := stacksP1[from][:move]
-        cratesToMoveP2 := stacksP2[from][:move]
-        //because they are LIFO
-        slices.Reverse(cratesToMoveP1)
+		cratesToMoveP2 := stacksP2[from][:move]
+		//because they are LIFO
+		slices.Reverse(cratesToMoveP1)
 		stacksP1[from] = stacksP1[from][move:]
-        stacksP2[from] = stacksP2[from][move:]
+		stacksP2[from] = stacksP2[from][move:]
 
 		stacksP1[to] = slices.Insert(stacksP1[to], 0, cratesToMoveP1...)
 		stacksP2[to] = slices.Insert(stacksP2[to], 0, cratesToMoveP2...)
 	}
 
 	stackKeys := maps.Keys(stacksP1)
-    slices.Sort(stackKeys)
-    var topOfStacksP1 string
-    var topOfStacksP2 string
+	slices.Sort(stackKeys)
+	var topOfStacksP1 string
+	var topOfStacksP2 string
 	for _, stack := range stackKeys {
-        topOfStacksP1 += stacksP1[stack][0]
-        topOfStacksP2 += stacksP2[stack][0]
+		topOfStacksP1 += stacksP1[stack][0]
+		topOfStacksP2 += stacksP2[stack][0]
 	}
-    fmt.Printf("Part 1: %v\nPart 2: %v\n", topOfStacksP1, topOfStacksP2)
+	fmt.Printf("Part 1: %v\nPart 2: %v\n", topOfStacksP1, topOfStacksP2)
 }
