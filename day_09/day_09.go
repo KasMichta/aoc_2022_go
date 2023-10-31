d330d30e993444c737349dccbcb060579e56ab0e
KasMichta <kacper.michta33@gmail.com>
2023-10-31 17:29:04 +0000

    part 2 complete, moveTail can probs be refactored

--- day_09/day_09.go
+++ day_09/day_09.go
@@ -44,25 +44,51 @@ func moveTail(h []int, t []int) {
 	} else {
 		if h[0] == t[0]+2 {
 			t[0]++
-			t[1] = h[1]
+			if h[1] > t[1] {
+				t[1]++
+			} else {
+				t[1]--
+			}
+
 		} else if h[0] == t[0]-2 {
 			t[0]--
-			t[1] = h[1]
+			if h[1] > t[1] {
+				t[1]++
+			} else {
+				t[1]--
+			}
 		} else if h[1] == t[1]+2 {
 			t[1]++
-			t[0] = h[0]
+			if h[0] > t[0] {
+				t[0]++
+			} else {
+				t[0]--
+			}
 		} else if h[1] == t[1]-2 {
 			t[1]--
-			t[0] = h[0]
+			if h[0] > t[0] {
+				t[0]++
+			} else {
+				t[0]--
+			}
 		}
 	}
 }
 
 func main() {
+	//Part 1
 	lines := readLines(os.Args[1])
 	head := []int{0, 0}
 	tail := []int{0, 0}
 	tailPositions := make(map[string]bool)
+
+	//Part 2
+	rope := make([][]int, 10)
+	for i := 0; i <= 9; i++ {
+		rope[i] = []int{0, 0}
+	}
+	ropeTailPositions := make(map[string]bool)
+
 	for _, line := range lines {
 		op := strings.Split(line, " ")
 		d := op[0]
@@ -71,22 +97,33 @@ func main() {
 			switch d {
 			case "L":
 				head[0]--
+				rope[0][0]--
 			case "R":
 				head[0]++
+				rope[0][0]++
 			case "U":
 				head[1]--
+				rope[0][1]--
 			case "D":
 				head[1]++
-
+				rope[0][1]++
 			}
 
-			if isTouching(head, tail) {
-				xy := fmt.Sprint(tail[0], tail[1])
-				tailPositions[xy] = true
-			} else {
+			if !isTouching(head, tail) {
 				moveTail(head, tail)
-				xy := fmt.Sprint(tail[0], tail[1])
-				tailPositions[xy] = true
+			}
+			xy := fmt.Sprint(tail[0], tail[1])
+			tailPositions[xy] = true
+
+			endOfRope := len(rope) - 1
+			for i := 0; i < endOfRope; i++ {
+				if !isTouching(rope[i], rope[i+1]) {
+					moveTail(rope[i], rope[i+1])
+				}
+				if i+1 == endOfRope {
+					xy := fmt.Sprint(rope[endOfRope][0], rope[endOfRope][1])
+					ropeTailPositions[xy] = true
+				}
 			}
 		}
 	}
@@ -97,5 +134,13 @@ func main() {
 			count++
 		}
 	}
+
+	ropeCount := 0
+	for _, v := range ropeTailPositions {
+		if v {
+			ropeCount++
+		}
+	}
 	fmt.Println("unique tail positions:", count)
+	fmt.Println("unique rope tail positions:", ropeCount)
 }
